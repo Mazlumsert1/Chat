2d1664c330b5ebd75f54d661ed6de3971cea2626
Lenovo <Lenovo@10.50.131.30>
2015-09-11 15:10:01 +0200

    Started on testing.

--- test/test/TestChat.java
+++ test/test/TestChat.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
 import java.io.IOException;
@@ -25,10 +20,10 @@ import server.Server;
  * @author Lenovo
  */
 public class TestChat {
-    
+
     public TestChat() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
         new Thread(new Runnable() {
@@ -42,38 +37,64 @@ public class TestChat {
             }
         }).start();
     }
-    
-    
+
     @AfterClass
     public static void tearDownClass() throws IOException {
-       Server.stopServer();
+        Server.stopServer();
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
-    
+
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
-     @Test
-     public void hello() throws IOException {
-         
-         Socket socket = new Socket("localhost" , 9090);
-         PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-         Scanner input = new Scanner(socket.getInputStream());
-         
-         // Testet om peter er i userlisten.
-         output.println("USER#peter");
-         String text = input.nextLine();
-         System.out.println(text);
-         Assert.assertEquals("UserList#peter" , text);
-         
-         
-     
-     }
+    @Test
+    public void hello() throws IOException {
+
+        Socket socket = new Socket("localhost", 9090);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        Scanner in = new Scanner(socket.getInputStream());
+
+        //------------------Testes om peter er i userlisten.-------------------
+        out.println("USER#peter");
+        String user = in.nextLine();
+        System.out.println(user);
+        Assert.assertEquals("USERLIST#[peter]", user);
+         //---------------------------------------------------------------------
+
+        //--------------Testes om beskeden bliver sendt til alle.--------------
+        out.println("MSG#*#Java");
+        String msg = in.nextLine();
+        System.out.println(msg);
+        Assert.assertEquals("MSG#*#Java", msg);
+        //---------------------------------------------------------------------
+
+        Socket socket2 = new Socket("localhost", 9090);
+        PrintWriter out2 = new PrintWriter(socket2.getOutputStream(), true);
+        Scanner in2 = new Scanner(socket.getInputStream());
+
+        out2.println("USER#hans");
+        String user1 = in.nextLine();
+        System.out.println(user1);
+        Assert.assertEquals("USERLIST#[peter,hans]#", user1);
+
+        out2.println("MSG#*#JavaScript");
+        String msg1 = in.nextLine();
+        System.out.println(msg1);
+        Assert.assertEquals("MSG#[peter,hans]#JavaScript", msg1);
+
+        //---------------Testes om en specifik bruger modtag beskeden----------
+        out.println("MSG#lars#hey");
+        String msg2 = in.nextLine();
+        System.out.println(msg1);
+        Assert.assertEquals("MSG#lars#hey", msg1);
+        //---------------------------------------------------------------------
+
+    }
 }
